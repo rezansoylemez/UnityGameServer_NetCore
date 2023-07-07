8fb4c98576773eb5106e471352a81e31d8a44241
Rezan-Soylemez <rezan.sm@gmail.com>
2023-07-07 17:03:48 +0300

    BugFixed - PlayerRepository

---
+++ gameServer/Application/Services/PlayerService/PlayerManager.cs
@@ -0,0 +1,47 @@
+﻿using Application.Repositories;
+using Domain.EntityModels;
+
+namespace Application.Services.PlayerService;
+
+public class PlayerManager : IPalyerService
+{
+    private readonly IPlayerRepository _playerRepository;
+
+    public PlayerManager(IPlayerRepository playerRepository)
+    {
+        _playerRepository = playerRepository;
+    }
+
+    public async Task<Player> Create(Player player)
+    {
+        var createdPlayer = await _playerRepository.InsertOneAsync(player);
+        return createdPlayer;
+    }
+
+    public async Task<Player> Delete(Player player)
+    {
+        var deleted = await _playerRepository.ReplaceOneAsync(document: player);
+        return deleted;
+
+    }
+    public async Task<Player> Update(Player player)
+    {
+        var updatedPlayer = await _playerRepository.ReplaceOneAsync(player);
+        return updatedPlayer;
+    }
+
+
+
+    public async Task<Player> GetById(string id)
+    {
+        var player = await _playerRepository.FindByIdAsync(id: id);
+        return player;
+    }
+
+    public async Task<Player> Remove(Player player)
+    {
+        var deleted = await _playerRepository.DeleteAsync(document: player);
+        return deleted;
+    }
+     
+}
