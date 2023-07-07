b95858e438f7157de781a0a1d4145c83f5f4da8a
Rezan-Soylemez <rezan.sm@gmail.com>
2023-07-07 16:51:57 +0300

    Featured - PlayerRepository And Folder Layer Structure Added

---
+++ gameServer/Application/Repositories/IPlayerRepository.cs
@@ -0,0 +1,9 @@
+﻿using Core.Persistence.Repositories.ReadRepositories;
+using Core.Persistence.Repositories.WriteRepositories;
+using Domain.EntityModels;
+
+namespace Application.Repositories;
+
+public interface IPlayerRepository : IWriteMongoDriverRepositoryBase<Player>, IReadMongoDriverRepositoryBase<Player>
+{
+}
---
+++ gameServer/Persistence/Repositories/PlayerRepository.cs
@@ -0,0 +1,14 @@
+﻿using Application.Repositories;
+using Core.Persistence.Repositories;
+using Core.Persistence.Settings;
+using Domain.EntityModels;
+using Microsoft.Extensions.Options;
+
+namespace Persistence.Repositories;
+public class PlayerRepository : MongoDriverRepositoryBase<Player>, IPlayerRepository
+{
+    public PlayerRepository(IOptions<DataBaseSettings> context) : base(context)
+    {
+
+    }
+}
