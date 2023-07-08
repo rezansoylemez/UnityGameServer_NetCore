d62adabf9ed38fbe92710e9487678b47d8a722b9
Rezan-Soylemez <rezan.sm@gmail.com>
2023-07-08 18:59:38 +0300

    Featured - GameEngine ReferenceAdded And JsonFormat

--- gameServer/Server/Program.cs
+++ gameServer/Server/Program.cs
@@ -1,10 +1,15 @@
 using Core.Persistence.Settings;
+using Newtonsoft.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson(o =>
+{
+    //PascalCase 
+    o.SerializerSettings.ContractResolver = new DefaultContractResolver();
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
