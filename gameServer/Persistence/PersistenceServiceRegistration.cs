652727d83d2f62d7eb744afa5c5c9746bc73b535
Rezan-Soylemez <rezan.sm@gmail.com>
2023-07-08 15:29:57 +0300

    Fetured - PersistenceRegistrationService (DI) Added

---
+++ gameServer/Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,29 @@
+﻿using Application.Repositories;
+using Core.Persistence.DbContext;
+using Core.Persistence.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Repositories;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        
+        //services.Configure<DataBaseSettings>(options =>
+        //{
+        //    options.ConnectionString = Configuration.GetSection("MongoConnection:ConnectionString").Value;
+        //    options.DatabaseName = Configuration.GetSection("MongoConnection:DatabaseName").Value;
+        //});
+        //services.AddDbContext<BaseDbContext>(
+        //options => options.UseSqlServer(configuration.GetConnectionString("FrostlineGamesWebConnection")
+        //)); 
+         
+        services.AddScoped<IPlayerRepository, PlayerRepository>();
+        
+
+
+
+        return services;
+    }
+}
